785e76e91b4d44e4d6f9dde377586965b0a896a2
eRy-sk <ride.poke@gmail.com>
2020-10-17 15:42:02 +0900

    refactoring

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,17 +3,13 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      self.current_user = find_verified_user
+      reject_unauthorized_connection unless find_verified_user
     end
 
     private
 
     def find_verified_user
-      session_key = cookies.encrypted[Rails.application.config.session_options[:key]]
-      verified_id = session_key['warden.user.user.key'][0][0]
-      verified_user = User.find_by(id: verified_id)
-      return reject_unauthorized_connection unless verified_user
-      verified_user
+      self.current_user = env['warden'].user
     end
   end
 end
